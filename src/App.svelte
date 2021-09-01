d239cb50920c4be2d717858934d095130da65d96
basic-bhavya <bjain.nd@gmail.com>
2021-09-01 15:45:31 +0530

    taking input

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,31 @@
 <script>
-	export let name;
+	let name = "Bro";
+	let src = "favicon.png";
+
+	const handleInput = (e) => {
+		name = e.target.value;
+	}
+
+	const resetname = () => {
+		name = "gasdfoasnifdiasf";
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name} </h1>
+	<input type="text" bind:value={name}>
+	<button on:click={resetname}>Set name to unknown</button>
+	<img {src} alt="favicon">
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		text-align: center;
 	}
 </style>
