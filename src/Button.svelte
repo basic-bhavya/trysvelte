c9658d237b181d0e0065e886b0fac0485e9016f4
basic-bhavya <bjain.nd@gmail.com>
2021-09-01 17:56:52 +0530

    event modifiers

--- src/Button.svelte
+++ src/Button.svelte
@@ -2,13 +2,18 @@
   export let btnText = "default texxt";
 </script>
 
-<div>
-  <span on:click> Click to log </span>
-  <button>{btnText}</button>
+<div class="card" on:click|self>
+  <span> Click to log </span>
+  <button on:click|once={() => console.log("once")}>{btnText}</button>
 </div>
 
 <!-- markup (zero or more items) goes here -->
 <style>
+  .card {
+    padding: 12px;
+    border: 1px solid violet;
+    color: white;
+  }
   button {
     display: flex;
     flex-direction: column;
